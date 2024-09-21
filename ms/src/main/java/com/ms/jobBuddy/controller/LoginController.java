87e4173df8fe32a13ba74177179c91d5dcbc7e16
Shreya Chivilkar <shreyasc1999@gmail.com>
2024-09-20 23:04:19 -0400

    Basic Skeleton

--- ms/src/main/java/com/ms/jobBuddy/controller/LoginController.java
+++ ms/src/main/java/com/ms/jobBuddy/controller/LoginController.java
@@ -1,4 +1,19 @@
 package com.ms.jobBuddy.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class LoginController {
+
+    @Operation(summary = "Get a user by ID", description = "Returns a single user based on the provided ID")
+    @GetMapping("/users")
+    public String getUserById() {
+        System.out.println("Hello User");
+        return "Hello User";
+    }
 }
+
