daf78f9bb7dff7c6da5f0e7fbbb620998ed64611
Shreya Chivilkar <shreyasc1999@gmail.com>
2024-09-20 21:58:47 -0400

    Basic skeleton

---
+++ ms/src/main/java/com/ms/jobBuddy/JobBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.ms.jobBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBuddyApplication.class, args);
+	}
+
+}
