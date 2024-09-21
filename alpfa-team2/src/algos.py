b5f2217b52c788afba4c1e29d4a6b6ed42cceab1
Soham Pati <sohampati2005@gmail.com>
2024-09-21 10:39:46 -0400

    initial algos - more coming

---
+++ alpfa-team2/src/algos.py
@@ -0,0 +1,19 @@
+# count, finds number of skills each employee has for each posting
+def count(sponsor, employee):
+    count = 0
+    set_sponsor = set(sponsor)
+    for skill in employee:
+        if skill in set_sponsor:
+            count += 1
+    
+    return count
+#based on custom employee threshold for match, returns if there is a match
+def isMatch(currentSkills, totalSkills, threshold):
+    if totalSkills == 0:  # Prevent division by zero
+        return False
+    return currentSkills / totalSkills >= threshold
+
+
+
+
+
